e2afcdc2f83c0d9fb9b39ce91b517c7fd0b2c8d0
kvengala <kvengala@hawk.iit.edu>
2015-09-28 16:19:21 -0500

    removed unwanted activity

--- tranql_app/Call_busy/app/src/main/java/com/trnql_devpost/kashyap/callbusy/MainActivity.java
+++ tranql_app/Call_busy/app/src/main/java/com/trnql_devpost/kashyap/callbusy/MainActivity.java
@@ -1,9 +1,16 @@
 package com.trnql_devpost.kashyap.callbusy;
 
 import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.trnql.smart.activity.ActivityEntry;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +25,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        CallbackManager callbackManager;
+        setContentView(R.layout.activity_login_activity);
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+
     }
     //@Override
     protected void smartActivityChange(ActivityEntry userActivity) {
